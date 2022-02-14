f7005996e58ce92ebd546aece97dff0bba844e3c
Patrick <patrick@meetpatrick.dev>
2022-02-13 23:24:38 -0600

    final changes for event listeners

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ uriTextEl.classList.add('uriTextEl')
 
 uriFormEl.addEventListener('keyup', handleUri)
 uriFormEl.addEventListener('change', handleUri)
+uriFormEl.addEventListener('paste', handleUri)
 let uriName = ''
 let unchangedUri = '';
 
